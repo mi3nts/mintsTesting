fc94c3dedef4aa50647038915913433bc3c4c9f3
lakithaomal <lhw150030@utdallas.edu>
2025-06-04 12:47:34 +0000

    edited typo

--- people/diegoMalagon/scd30_smbus.py
+++ people/diegoMalagon/scd30_smbus.py
@@ -12,9 +12,9 @@ CMD_TEMP_OFFSET = 0x5403 # argument needed
 CMD_ALT_COMP = 0x5102 # argument
 CMD_SET_FRC = 0x5204 #argument neeeded
 
-	def word_to_float(msb_word, lsb_word):
-        raw_bytes = struct.pack('>HH', msb_word, lsb_word)
-        return struct.unpack('>f', raw_bytes)[0]
+def word_to_float(msb_word, lsb_word):
+    raw_bytes = struct.pack('>HH', msb_word, lsb_word)
+    return struct.unpack('>f', raw_bytes)[0]
 
 class SCD30:
     def __init__(self, bus=5):
