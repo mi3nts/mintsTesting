8dd813f5aaf7ee8fa06094cd4dd86ff5aa94e1e3
lakithaomal <lhw150030@utdallas.edu>
2025-06-16 21:55:26 +0000

    updated INA 219 file

--- people/diegoMalagon/INA219.py
+++ people/diegoMalagon/INA219.py
@@ -5,22 +5,42 @@ from ina219 import INA219
 from ina219 import DeviceRangeError
 
 
-class SMBusINA219:
+class firstINA219:
+
     def __init__(self, bus=5, address=0x40):
         self.bus = SMBus(bus)
         self.address = address
 
 
     def read():
-        ina = INA219(0.1)
+        ina = INA219(0.1,0x40)
         ina.configure()
 
-        print("Bus Voltage: %.3f V" % ina.bus_voltage())
+        print("Bus Voltage: %.3f V" % ina.voltage())
         try:
             print("Power: %.3f mA" % ina.power())
             print("Shunt Voltage: %.3f mV" % ina.shunt_voltage())
             print('Bus Current: %.3f' % ina.current())
-            print('Just Voltage: %3.f' & ina.voltage())
+
+
+        except DeviceRangeError as e:
+            print (e)
+
+class secondINA219:
+    def __init__(self, bus=5, address=0x41):
+        self.bus = SMBus(bus)
+        self.address = address
+
+    def read():
+        inaTwo = INA219(0.1,0x41)
+        inaTwo.configure()
+
+        print("Bus Voltage: %.3f V" % inaTwo.voltage())
+        try:
+            print("Power: %.3f mA" % inaTwo.power())
+            print("Shunt Voltage: %.3f mV" % inaTwo.shunt_voltage())
+            print('Bus Current: %.3f' % inaTwo.current())
+            print('Just Voltage: %.3f' & inaTwo.voltage())
 
         except DeviceRangeError as e:
             print (e)
