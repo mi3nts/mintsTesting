a995dc04e36cb035475d8cb3d137333693a5e6e0
DiegoMalagn <dmalagon250@gmail.com>
2025-07-02 14:11:24 -0500

    'added main file for bme280'

---
+++ people/diegoMalagon/firmwareTesting/mainBme280.py
@@ -0,0 +1,25 @@
+from time import sleep
+from bme280_driver import BME280  
+
+def main():
+    sensor = BME280()
+
+    try:
+        while True:
+            sensor.sample()
+            # print all measurements
+            sensor.print()
+            print("----------")
+            # optionally print individually
+            sensor.print("temperature")
+            sensor.print("pressure")
+            sensor.print("humidity")
+
+            # sample every 5 seconds
+            sleep(5)
+
+    except KeyboardInterrupt:
+        print("\nExiting program.")
+
+if __name__ == "__main__":
+    main()
